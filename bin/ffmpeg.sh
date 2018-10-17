4ac213982dd515a6e5ef7d151d58bf423ffe9a58
nikonyrh <niko.nyrhila@gmail.com>
2018-10-18 00:24:03 +0300

    Added CUDA support to my ffmpeg wrapper, how cool is that? :o

--- bin/ffmpeg.sh
+++ bin/ffmpeg.sh
@@ -4,7 +4,8 @@ VIDEO=$1
 AUDIO=$2
 shift 2
 
-FPS=60
+FPS=
+CUDA=
 CODEC=x264
 RES=
 TARGET_DIR=.
@@ -13,10 +14,14 @@ CONTINUE=1
 while (( "$#" )) && [ $CONTINUE = 1 ]; do
     case "$1" in
         "--fps")
-            FPS=$2
+            FPS="-r $2"
             shift 2
             ;;
         "--codec")
+            if [ "$CUDA" != "" ]; then
+                >&2 echo "--codec must be defined before -cuda flag!" && exit 1
+            fi
+            
             CODEC=$2
             shift 2
             ;;
@@ -28,14 +33,34 @@ while (( "$#" )) && [ $CONTINUE = 1 ]; do
             TARGET_DIR=$2
             shift 2
             ;;
+        "-cuda")
+            CUDA='-hwaccel cuvid'
+            
+            if [ "$CODEC" == x264 ]; then
+                CODEC=h264_nvenc
+            else
+                CODEC=hevc_nvenc
+            fi
+            
+            shift
+            ;;
         *)
             CONTINUE=0
     esac
 done
 
+if [ "$CUDA" = "" ]; then
+    ARGS="-vcodec lib$CODEC -strict -2 -crf $VIDEO -movflags faststart -b:a ${AUDIO}K $FPS $RES"
+else
+    VIDEO_MIN=$(($VIDEO - 1))
+    VIDEO_MAX=$(($VIDEO + 1))
+    
+    # ref. http://ntown.at/de/knowledgebase/cuda-gpu-accelerated-h264-h265-hevc-video-encoding-with-ffmpeg/
+    ARGS="-c:v $CODEC -rc vbr_hq -qmin:v $VIDEO_MIN -qmax:v $VIDEO_MAX -pix_fmt yuv420p -movflags faststart -b:a ${AUDIO}K $FPS $RES"
+fi
 
-ARGS="-vcodec lib$CODEC -strict -2 -crf $VIDEO -movflags faststart -b:a ${AUDIO}K -r $FPS $RES"
 # echo $ARGS && exit 0
+
 
 while (( "$#" )); do
     fname="$1"
@@ -54,16 +79,7 @@ while (( "$#" )); do
         continue
     fi
     
-    ffmpeg -i "$fname" $ARGS "$out"
-    
-    #rm -f "$out"
-    
-    #ffmpeg -i "$fname" -vcodec libx264 -strict -2 -crf 18 -b:a 192K "$out"
-    #ffmpeg -i "$fname" -vcodec libx264 -strict -2 -crf 21 -b:a 192K "$out"
-    #ffmpeg -i "$fname" -vcodec libx264 -strict -2 -crf 23 -b:a 192K "$out"
-    #ffmpeg -i "$fname" -vcodec libx264 -strict -2 -crf 25 -b:a 92K "$out"
-    
-    #ffmpeg -i "$fname" -vcodec libx264 -strict -2 -vf scale=1920:-1 -crf 25 -b:a 128K "$out"
+    ffmpeg $CUDA -i "$fname" $ARGS "$out"
     
     #ffmpeg -i "$fname" -vf hqdn3d=7 -vcodec libx264 -strict -2 -crf 23 -b:a 192K "$out"
     #ffmpeg -i "$fname" -vf transpose=1 -vcodec libx264 -strict -2 -crf 18 -b:a 192K "$out"
