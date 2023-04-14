3fd48498905c4c687a6498b26e6c5050e84b1faf
NikoNyrh <nikonyrh@users.noreply.github.com>
2023-04-14 11:47:27 +0300

    Update s3cp.sh

--- bin/s3cp.sh
+++ bin/s3cp.sh
@@ -5,7 +5,6 @@ while (( "$#" )); do
     F=`echo "$1" | sed -r 's_.+/__'`
     echo "https://s3-eu-west-1.amazonaws.com/$D$F"
     
-    aws s3 cp --storage-class REDUCED_REDUNDANCY "$1" "s3://$D" --acl public-read
+    aws s3 cp --storage-class STANDARD_IA "$1" "s3://$D" --acl public-read
     shift
 done
-
